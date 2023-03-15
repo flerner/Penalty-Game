69e65a81886f5da27513837533a63bbed30f755e
flerner <felix.lerner01@gmail.com>
2023-03-15 20:47:48 -0300

    Slowing down balls

--- Penalty Game/Assets/Scripts/Ball2.cs
+++ Penalty Game/Assets/Scripts/Ball2.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball2 : MonoBehaviour
 {
+    float drag = 0.4f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,19 @@ public class Ball2 : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        GraduallyStopTheBall();
+    }
+    private void GraduallyStopTheBall()
+    {
+
+        this.transform.GetComponent<Rigidbody2D>().velocity -= this.transform.GetComponent<Rigidbody2D>().velocity * drag * Time.deltaTime;
+        if (Math.Abs(this.transform.GetComponent<Rigidbody2D>().velocity.x) < 0.2f || Math.Abs(this.transform.GetComponent<Rigidbody2D>().velocity.y) < 0.2f)
+        {
+            this.transform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
     }
 }
