e9543c454c749095509619788c820a0f862ae3b4
flerner <felix.lerner01@gmail.com>
2023-03-15 14:17:13 -0300

    Ball movement

---
+++ Penalty Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Ball : MonoBehaviour
+{
+    Vector2 startPosition, endPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(startPosition);
+        }
+        if(Input.GetMouseButtonUp(0)) 
+        {
+            endPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            this.transform.GetComponent<Rigidbody2D>().AddForce((startPosition - endPosition) * 50 , ForceMode2D.Force);
+            Debug.Log(endPosition);
+        }
+    }
+}
