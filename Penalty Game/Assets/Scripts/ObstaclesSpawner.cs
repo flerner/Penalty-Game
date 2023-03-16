3502cb0fae164802e9d7ca083d670af3bebe54bc
flerner <felix.lerner01@gmail.com>
2023-03-16 13:03:46 -0300

    Obstacles added

---
+++ Penalty Game/Assets/Scripts/ObstaclesSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ObstaclesSpawner : MonoBehaviour
+{
+    private Transform obstaclesSpawner;
+    private Transform[] spawnPoints;
+    [SerializeField]
+    private GameObject obstacle;
+
+    private void Awake()
+    {
+        obstaclesSpawner = GetComponent<Transform>();
+        spawnPoints = obstaclesSpawner.GetComponentsInChildren<Transform>().Skip(1).ToArray();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void SpawnObstacles()
+    {
+        Instantiate(obstacle, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+    }
+}
