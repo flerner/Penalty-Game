58bccc57c2972f47c33cbe3ff8046808ce258838
flerner <felix.lerner01@gmail.com>
2023-03-16 13:14:31 -0300

    map resizing

--- Penalty Game/Assets/Scripts/Goal.cs
+++ Penalty Game/Assets/Scripts/Goal.cs
@@ -15,7 +15,7 @@ public class Goal : MonoBehaviour
     {
         
     }
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("Gol!");
     }
